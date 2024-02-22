34ce398a62d4a70f3918d27f85840e554d327853
ssslowl <ssslowl2005@gmail.com>
2024-02-22 15:24:04 +0600

    New changes

---
+++ src/main/java/com/tattistom/clinic/ClientSystem/ClientSystemApplication.java
@@ -0,0 +1,13 @@
+package com.tattistom.clinic.ClientSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientSystemApplication.class, args);
+	}
+
+}
